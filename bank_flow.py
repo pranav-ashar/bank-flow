a125f8eb3880e91c6701e7ea311df7bebfbb2982
Pranav Ashar <pranav.ashar11@gmail.com>
2026-07-15 20:41:04 +0530

    Changes and Upgrade Menu

--- bank_flow.py
+++ bank_flow.py
@@ -37,55 +37,99 @@ bank_records = {
     }
 }
 
-#Login
+transactions = {
+    123456789101 : 
+    {
+    },
+    
+    123456789102 : 
+    {
+    },
+    
+    123456789103 : 
+    {
+    },
+    
+    123456789104 : 
+    {
+    },
+    
+    123456789105 : 
+    {
+    }
+}
+
+            
 def login():
-    account_num = int(input("Enter Account Number : "))
-    if account_num in bank_records:
-        return account_num
-    else:
-        print("Invalid Account Number")
-        
-
-
+    while True:
+        global account_num
+        account_num = int(input("Enter Account Number : "))
+        if account_num in bank_records:
+            
+            print()
+            print(f"Welcome, {bank_records[account_num]["name"]}")
+            print()
+            menu(account_num)
+            
+            
+        else:
+            print("Invalid Account Number")
+            print()
 
 
 #Account Balance
 def acc_bal(acc_no):
+    acc_no = account_num
+    
+    print("-------------------")
     print("ACCOUNT BALANCE")
     print(f"{bank_records[acc_no]["name"]}")
     print(f"Balance : $ {bank_records[acc_no]["balance"]}")
+    print("-------------------")
+    operation()
 
 #Deposit
 def deposit(acc_no):
+    print("-------------------")
     deposit_amt = int(input("Enter Amount : "))
     if (deposit_amt > 0):
         balance_new = (bank_records[acc_no]["balance"]) + deposit_amt
         bank_records[acc_no].update({"balance" : balance_new})
+        transactions[acc_no].update({"Deposited : + " : deposit_amt})
         print(f"The Amount of $ {deposit_amt} has added succesfully to your account")
-        print()
+        print("-------------------")
+        operation()
     else:
         print("Invalid Amount Entered")
+        print("-------------------")
     
 #Withdrawl    
-def withdrawl(acc_no):
+def withdraw(acc_no):
+    print("-------------------")
     withdrawl_amt = int(input("Enter Amount : "))
     balance_new = (bank_records[acc_no]["balance"]) - withdrawl_amt
     if (balance_new >= 500):
         bank_records[acc_no].update({"balance" : balance_new})
         print(f"The Amount of $ {withdrawl_amt} has succesfully withdrawn from your account")
-        print()
+        print("-------------------")
+        operation()
     else:
         print("Insufficient Balance")
+        print("-------------------")
     
 #Pin Change
 def pin_change(acc_no):
+    print("-------------------")
     prev_pin = int(input("Enter Previous Pin : "))
     if(prev_pin == bank_records[acc_no]["pin"]):
         new_pin = input("Enter New Pin : ")
         bank_records[acc_no].update({"pin" : new_pin})
         print("The Pin has been Updated")
+        print("-------------------")
+        operation()
     else:
         print("Invalid Pin")
+        print("-------------------")
 
 #Transfer Money
 def transfer_money(acc_no):
@@ -100,6 +144,8 @@ def transfer_money(acc_no):
                 bank_records[transfer_acc].update({"balance":balance_new_trasferee})
                 bank_records[acc_no].update({"balance":balance_new_transferer})
                 print(f"The amount of $ {transfer_amt} has been transferred to {bank_records[transfer_acc]["name"]}")
+                print()
+                operation()
             else:
                 print("Insufficient Balance")
         else:
@@ -107,14 +153,9 @@ def transfer_money(acc_no):
     else:
         print("Incorrect Account Number")
         
-#Program Run
-account_num = login()
-print()
-print(f"Welcome, {bank_records[account_num]["name"]}")
-print()
-
 #Menu
-while True:
+def menu(acc_no):
+    acc_no = account_num
     print("""-------ATM-------
 1) Check Balance
 2) Deposit Money 
@@ -126,22 +167,51 @@ while True:
 -----------------
 """)
     user_response = int(input("User Response : "))
+    print()
     if(user_response == 1):
-        print("Check Balance")
+        acc_bal(acc_no)
     elif(user_response == 2):
-        print("Deposit")
+        deposit(acc_no)
     elif(user_response == 3):
-        print("Withdraw Money")
+        withdraw(acc_no)
     elif(user_response == 4):
-        print("Transfer Money")
+        transfer_money(acc_no)
     elif(user_response == 5):
-        print("Mini Statement")
+        print()
     elif(user_response == 6):
-        print("Change Pin")
+        pin_change(acc_no)
     elif(user_response == 7):
-        print("Withdraw Money")
+        logout()
     else:
         print("Invalid Response")
     
- 
+        
+def operation():
+    
+    print("""
+Would you like another transaction?
+
+1) Yes
+2) Logout """)
+    print()
+    user_response = int(input("User Response : "))
+    if(user_response == 1):
+        print()
+        menu(account_num)
+    elif(user_response == 2):
+            print()
+            logout()
+    else:
+        print("Invalid Response")
+        
+def logout():
+    login()
+        
+        
+
+#Program Run
+
+login()
+
+
 
